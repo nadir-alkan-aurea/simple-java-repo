78f560e68f9bf43b4deec50042c012d089a14fe8
Alaa Nassef <alaa.nassef@aurea.com>
2019-10-10 14:07:09 +0200

    First real commit

---
+++ src/main/java/com/devfactory/SimpleClass.java
@@ -0,0 +1,8 @@
+package com.devfactory;
+
+public class SimpleClass {
+
+    public static void main(String[] args) {
+        System.out.println("Even though I'm simple, it doesn't mean I should be ignored");
+    }
+}
